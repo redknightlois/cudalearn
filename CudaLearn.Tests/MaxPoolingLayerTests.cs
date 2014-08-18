37c1a6280c6db56452c6219455b5dff6bd8ebc73
Federico <federico.lois@corvalius.com>
2014-08-17 21:12:44 -0300

    - First unoptimized implementation of the MaxPoolingLayer.

---
+++ CudaLearn.Tests/MaxPoolingLayerTests.cs
@@ -0,0 +1,334 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Extensions;
+
+namespace CudaLearn.Tests
+{
+
+    public class MaxPoolingLayerTests
+    {
+        private readonly Blob bottom = new Blob(2, 3, 6, 5);
+        private readonly Blob top = new Blob();
+
+        public MaxPoolingLayerTests()
+        {
+            var filler = new GaussianFiller();
+            filler.Fill(bottom);
+        }
+
+        [Fact]
+        public void MaxPoolingLayer_Setup()
+        {
+            var layer = new MaxPoolingLayer(3, 2);
+            layer.Setup(bottom, top);
+
+            Assert.Equal(bottom.Num, top.Num);
+            Assert.Equal(bottom.Channels, top.Channels);
+            Assert.Equal(3, top.Height);
+            Assert.Equal(2, top.Width);
+        }
+
+        [Fact]
+        public void MaxPoolingLayer_SetupPadded()
+        {
+            var layer = new MaxPoolingLayer(3, 2, 1);
+            layer.Setup(bottom, top);
+
+            Assert.Equal(bottom.Num, top.Num);
+            Assert.Equal(bottom.Channels, top.Channels);
+            Assert.Equal(4, top.Height);
+            Assert.Equal(3, top.Width);
+        }
+
+        [Theory]
+        [InlineData(1), InlineData(2)]
+        public void MaxPoolingLayer_ForwardRectangularWithSquareKernel(int topLayer)
+        {
+            const int num = 2;
+            const int channels = 2;
+            var bottom = new Blob(num, channels, 3, 5);
+
+            var topList = new Blob[topLayer];
+            for (int i = 0; i < topLayer; i++)
+                topList[i] = new Blob();
+
+            // Input: 2x 2 channels of:
+            //     [1 2 5 2 3]
+            //     [9 4 1 4 8]
+            //     [1 2 5 2 3]
+
+            var bottomData = bottom.Data;
+            for (int i = 0; i < 15 * num * channels; i += 15)
+            {                
+                bottomData[i + 0] = 1;
+                bottomData[i + 1] = 2;
+                bottomData[i + 2] = 5;
+                bottomData[i + 3] = 2;
+                bottomData[i + 4] = 3;
+                bottomData[i + 5] = 9;
+                bottomData[i + 6] = 4;
+                bottomData[i + 7] = 1;
+                bottomData[i + 8] = 4;
+                bottomData[i + 9] = 8;
+                bottomData[i + 10] = 1;
+                bottomData[i + 11] = 2;
+                bottomData[i + 12] = 5;
+                bottomData[i + 13] = 2;
+                bottomData[i + 14] = 3;
+            }
+
+            var layer = new MaxPoolingLayer(2, 1, 0);
+            layer.Setup(new[] { bottom }, topList);
+
+            foreach ( var top in topList)
+            {
+                Assert.Equal(num, top.Num);
+                Assert.Equal(channels, top.Channels);
+                Assert.Equal(2, top.Height);
+                Assert.Equal(4, top.Width);
+            }
+
+            layer.Forward(new[] { bottom }, topList);
+
+            // Expected output: 2x 2 channels of:
+            //     [9 5 5 8]
+            //     [9 5 5 8]
+            for (int i = 0; i < 8 * num * channels; i += 8)
+            {
+                var topData = topList[0].Data;
+                Assert.Equal(9,topData[i + 0]);
+                Assert.Equal(5,topData[i + 1]);
+                Assert.Equal(5,topData[i + 2]);
+                Assert.Equal(8,topData[i + 3]);
+                Assert.Equal(9,topData[i + 4]);
+                Assert.Equal(5,topData[i + 5]);
+                Assert.Equal(5,topData[i + 6]);
+                Assert.Equal(8,topData[i + 7]);
+            }
+
+            if ( topList.Length > 1 )
+            {
+                // Expected mask output: 2x 2 channels of:
+                //     [5  2  2 9]
+                //     [5 12 12 9]
+                for (int i = 0; i < 8 * num * channels; i += 8)
+                {
+                    var topData = topList[1].Data;
+                    Assert.Equal(5, topData[i + 0]);
+                    Assert.Equal(2, topData[i + 1]);
+                    Assert.Equal(2, topData[i + 2]);
+                    Assert.Equal(9, topData[i + 3]);
+                    Assert.Equal(5, topData[i + 4]);
+                    Assert.Equal(12, topData[i + 5]);
+                    Assert.Equal(12, topData[i + 6]);
+                    Assert.Equal(9, topData[i + 7]);
+                }
+            }
+        }
+
+        [Theory]
+        [InlineData(1), InlineData(2)]
+        public void MaxPoolingLayer_BackwardsRectangularWithSquareKernelConstant(int topLayer)
+        {
+            const int num = 2;
+            const int channels = 2;
+            var bottom = new Blob(num, channels, 3, 5);
+
+            var topList = new Blob[topLayer];
+            for (int i = 0; i < topLayer; i++)
+                topList[i] = new Blob();
+
+            var filler = new ConstantFiller(2);
+            filler.Fill(bottom);
+
+            // Input: 2x 2 channels of:
+            //     [2 2 2 2 2]
+            //     [2 2 2 2 2]
+            //     [2 2 2 2 2]
+
+            var layer = new MaxPoolingLayer(2, 1, 0);
+            layer.Setup(new[] { bottom }, topList);
+            layer.Forward(new[] { bottom }, topList);
+
+            // Input: 2x 2 channels of:
+            //     [1 0 0 0]
+            //     [0 0 0 1]
+
+            for (int i = 0; i < 8 * num * channels; i += 8)
+            {
+                var topDiff = topList[0].Diff;
+                topDiff[i + 0] = 1;
+                topDiff[i + 7] = 1;
+            }
+
+            // Input: 2x 2 channels of:
+            //     [1 1 0 0 0]
+            //     [1 1 0 1 1]
+            //     [0 0 0 1 1]
+
+            layer.Backward(topList, new[] { true }, new[] { bottom });
+
+            for (int i = 0; i < 15 * num * channels; i += 15)
+            {
+                var bottomDiff = bottom.Diff;
+                Assert.Equal(1, bottomDiff[i + 0]);
+                Assert.Equal(1, bottomDiff[i + 1]);
+                Assert.Equal(0, bottomDiff[i + 2]);
+                Assert.Equal(0, bottomDiff[i + 3]);
+                Assert.Equal(0, bottomDiff[i + 4]);
+                Assert.Equal(1, bottomDiff[i + 5]);
+                Assert.Equal(1, bottomDiff[i + 6]);
+                Assert.Equal(0, bottomDiff[i + 7]);
+                Assert.Equal(1, bottomDiff[i + 8]);
+                Assert.Equal(1, bottomDiff[i + 9]);
+                Assert.Equal(0, bottomDiff[i + 10]);
+                Assert.Equal(0, bottomDiff[i + 11]);
+                Assert.Equal(0, bottomDiff[i + 12]);
+                Assert.Equal(1, bottomDiff[i + 13]);
+                Assert.Equal(1, bottomDiff[i + 14]);
+            }
+        }
+
+
+        [Theory]
+        [InlineData(1), InlineData(2)]
+        public void MaxPoolingLayer_BackwardsRectangularWithSquareKernelConstantStrips(int topLayer)
+        {
+            const int num = 2;
+            const int channels = 2;
+            var bottom = new Blob(num, channels, 3, 5);
+
+            var topList = new Blob[topLayer];
+            for (int i = 0; i < topLayer; i++)
+                topList[i] = new Blob();
+
+            var filler = new ConstantFiller(2);
+            filler.Fill(bottom);
+
+            // Input: 2x 2 channels of:
+            //     [2 1 2 1 2]
+            //     [2 1 2 1 2]
+            //     [2 1 2 1 2]
+
+            for (int i = 0; i < 15 * num * channels; i += 15)
+            {
+                var bottomData = bottom.Data;
+
+                bottomData[i + 1] = bottomData[i + 3] = 1;
+                bottomData[i + 6] = bottomData[i + 8] = 1;
+                bottomData[i + 11] = bottomData[i + 13] = 1;
+            }
+
+            var layer = new MaxPoolingLayer(2, 1, 0);
+            layer.Setup(new[] { bottom }, topList);
+            layer.Forward(new[] { bottom }, topList);
+
+            // Input: 2x 2 channels of:
+            //     [1 0 0 0]
+            //     [0 0 0 1]
+
+            for (int i = 0; i < 8 * num * channels; i += 8)
+            {
+                var topDiff = topList[0].Diff;
+                topDiff[i + 0] = 1;
+                topDiff[i + 7] = 1;
+            }
+
+            // Input: 2x 2 channels of:
+            //     [1 0 0 0 0]
+            //     [1 0 0 0 1]
+            //     [0 0 0 0 1]
+
+            layer.Backward(topList, new[] { true }, new[] { bottom });
+
+            for (int i = 0; i < 15 * num * channels; i += 15)
+            {
+                var bottomDiff = bottom.Diff;
+                Assert.Equal(1, bottomDiff[i + 0]);
+                Assert.Equal(0, bottomDiff[i + 1]);
+                Assert.Equal(0, bottomDiff[i + 2]);
+                Assert.Equal(0, bottomDiff[i + 3]);
+                Assert.Equal(0, bottomDiff[i + 4]);
+                Assert.Equal(1, bottomDiff[i + 5]);
+                Assert.Equal(0, bottomDiff[i + 6]);
+                Assert.Equal(0, bottomDiff[i + 7]);
+                Assert.Equal(0, bottomDiff[i + 8]);
+                Assert.Equal(1, bottomDiff[i + 9]);
+                Assert.Equal(0, bottomDiff[i + 10]);
+                Assert.Equal(0, bottomDiff[i + 11]);
+                Assert.Equal(0, bottomDiff[i + 12]);
+                Assert.Equal(0, bottomDiff[i + 13]);
+                Assert.Equal(1, bottomDiff[i + 14]);
+            }
+        }
+
+        [Theory]
+        [InlineData(1), InlineData(2)]
+        public void MaxPoolingLayer_BackwardsRectangularWithSquareKernel(int topLayer)
+        {
+            const int num = 2;
+            const int channels = 2;
+            var bottom = new Blob(num, channels, 3, 5);
+
+            var topList = new Blob[topLayer];
+            for (int i = 0; i < topLayer; i++)
+                topList[i] = new Blob();
+
+            var filler = new ConstantFiller(2);
+            filler.Fill(bottom);
+
+            // Input: 2x 2 channels of:
+            //     [2 2 2 2 2]
+            //     [2 2 2 2 2]
+            //     [2 2 2 2 2]
+
+            var layer = new MaxPoolingLayer(2, 1, 0);
+            layer.Setup(new[] { bottom }, topList);
+            layer.Forward(new[] { bottom }, topList);
+
+            // Input: 2x 2 channels of:
+            //     [1 1 1 1]
+            //     [0 0 0 0]
+
+            for (int i = 0; i < 8 * num * channels; i += 8)
+            {
+                var topDiff = topList[0].Diff;
+                topDiff[i + 0] = 1;
+                topDiff[i + 1] = 1;
+                topDiff[i + 2] = 1;
+                topDiff[i + 3] = 1;
+            }
+
+            // Input: 2x 2 channels of:
+            //     [1 2 2 2 1]
+            //     [1 2 2 2 1]
+            //     [0 0 0 0 0]
+
+            layer.Backward(topList, new[] { true }, new[] { bottom });
+
+            for (int i = 0; i < 15 * num * channels; i += 15)
+            {
+                var bottomDiff = bottom.Diff;
+                Assert.Equal(1, bottomDiff[i + 0]);
+                Assert.Equal(2, bottomDiff[i + 1]);
+                Assert.Equal(2, bottomDiff[i + 2]);
+                Assert.Equal(2, bottomDiff[i + 3]);
+                Assert.Equal(1, bottomDiff[i + 4]);
+                Assert.Equal(1, bottomDiff[i + 5]);
+                Assert.Equal(2, bottomDiff[i + 6]);
+                Assert.Equal(2, bottomDiff[i + 7]);
+                Assert.Equal(2, bottomDiff[i + 8]);
+                Assert.Equal(1, bottomDiff[i + 9]);
+                Assert.Equal(0, bottomDiff[i + 10]);
+                Assert.Equal(0, bottomDiff[i + 11]);
+                Assert.Equal(0, bottomDiff[i + 12]);
+                Assert.Equal(0, bottomDiff[i + 13]);
+                Assert.Equal(0, bottomDiff[i + 14]);
+            }
+        }
+    }
+}
---
+++ CudaLearn/PoolingLayer.Max.cs
@@ -0,0 +1,219 @@
+﻿using MathNet.Numerics.LinearAlgebra;
+using Seterlund.CodeGuard;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CudaLearn
+{
+    public class MaxPoolingLayerConfiguration : PoolingLayerConfiguration
+    {
+        public MaxPoolingLayerConfiguration(int kernelSize, int stride = 1, int padding = 0)
+            : this(new Size(kernelSize, kernelSize), new Size(stride, stride), new Size(padding, padding))
+        { }
+
+        public MaxPoolingLayerConfiguration(Size kernel, Size stride, Size padding = default(Size))
+            : base(LayerType.MaxPooling)
+        {
+            this.Kernel = kernel;
+            this.Stride = stride;
+            this.Padding = padding;
+        }
+
+        public MaxPoolingLayerConfiguration()
+            : base(LayerType.MaxPooling)
+        { }
+    }
+
+    public class MaxPoolingLayer : PoolingLayer<MaxPoolingLayerConfiguration>
+    {
+        private Vector<float> maxIndexes;
+
+        public MaxPoolingLayer(int kernelSize, int stride = 1, int padding = 0)
+            : this(new MaxPoolingLayerConfiguration(kernelSize, stride, padding))
+        { }
+
+        public MaxPoolingLayer(Size kernel, Size stride, Size padding = default(Size))
+            : this(new MaxPoolingLayerConfiguration(kernel, stride, padding))
+        { }
+
+        public MaxPoolingLayer()
+            : this(new MaxPoolingLayerConfiguration())
+        { }
+
+        public MaxPoolingLayer(MaxPoolingLayerConfiguration config)
+            : base(config)
+        { }
+
+        public override void Setup(IList<Blob> bottom, IList<Blob> top)
+        {
+            CheckSizeParameters();
+
+            this._maxTopBlobs = 2;
+
+            base.Setup(bottom, top);
+
+            int num = bottom[0].Num;
+            int channels = bottom[0].Channels;
+
+            foreach( var item in top )
+                item.Reshape(num, channels, Pooled.Height, Pooled.Width);
+
+            this.maxIndexes = Vector<float>.Build.SameAs(top[0].Data);
+        }
+
+        private void CheckSizeParameters()
+        {
+            Guard.That(() => this.Parameters.Kernel.Width).IsPositive();
+            Guard.That(() => this.Parameters.Kernel.Height).IsPositive();
+            Guard.That(() => this.Parameters.Kernel.Depth).IsEqual(0);
+
+            Guard.That(() => this.Parameters.Stride.Width).IsPositive();
+            Guard.That(() => this.Parameters.Stride.Height).IsPositive();
+            Guard.That(() => this.Parameters.Stride.Depth).IsEqual(0);
+
+            Guard.That(() => this.Parameters.Padding.Width).IsGreaterOrEqualThan(0);
+            Guard.That(() => this.Parameters.Padding.Height).IsGreaterOrEqualThan(0);
+            Guard.That(() => this.Parameters.Padding.Depth).IsEqual(0);
+        }
+
+        protected override float ForwardCpu(IList<Blob> bottom, IList<Blob> top)
+        {
+            var bottomData = bottom[0].Data;
+            var topData = top[0].Data;
+
+            int num = bottom[0].Num;
+            int channels = bottom[0].Channels;
+            int height = bottom[0].Height;
+            int width = bottom[0].Width;
+
+            Size padding = this.Parameters.Padding;
+            Size stride = this.Parameters.Stride;
+            Size kernel = this.Parameters.Kernel;
+
+            // Initialize 
+            Vector<float> outputMask;
+
+            bool useTopMask = top.Count > 1;
+            if ( useTopMask )
+                outputMask = top[1].Data;
+            else
+                outputMask = this.maxIndexes;
+
+            outputMask.Map(x => -1f, outputMask, Zeros.Include);
+            topData.Map(x => float.MinValue, topData, Zeros.Include);
+
+            // The main loop
+            int bottomOffset = 0;
+            int topOffset = 0;
+            for (int n = 0; n < num; n++)
+            {
+                for (int c = 0; c < channels; c++)
+                {
+                    for (int ph = 0; ph < Pooled.Height; ph++)
+                    {
+                        for (int pw = 0; pw < Pooled.Width; pw++)
+                        {
+                            int hstart = ph * stride.Height - padding.Height;
+                            int wstart = pw * stride.Width - padding.Width;
+                            int hend = Math.Min(hstart + kernel.Height, height + padding.Height);
+                            int wend = Math.Min(wstart + kernel.Width, width + padding.Width);
+                            int pool_size = (hend - hstart) * (wend - wstart);
+
+                            hstart = Math.Max(hstart, 0);
+                            wstart = Math.Max(wstart, 0);
+                            hend = Math.Min(hend, height);
+                            wend = Math.Min(wend, width);
+
+                            int poolIndex = ph * Pooled.Width + pw;
+                            for (int h = hstart; h < hend; h++)
+                            {
+                                for (int w = wstart; w < wend; w++)
+                                {
+                                    int index = h * width + w;
+                                    if (bottomData[bottomOffset + index] > topData[topOffset + poolIndex])
+                                    {
+                                        topData[topOffset + poolIndex] = bottomData[bottomOffset + index];
+                                        outputMask[topOffset + poolIndex] = index;
+                                    }
+                                }                                    
+                            }
+                        }
+                    }
+
+                    bottomOffset += bottom[0].Offset(0, 1);
+                    topOffset += top[0].Offset(0, 1);
+                }
+            }
+
+            return 0;  
+        }
+
+        protected override void BackwardCpu(IList<Blob> top, IList<bool> propagateDown, IList<Blob> bottom)
+        {
+            if (!propagateDown[0])
+                return;
+
+            var bottomData = bottom[0].Data;
+            var bottomDiff = bottom[0].Diff;
+            var topData = top[0].Data;
+            var topDiff = top[0].Diff;
+
+            int count = bottom[0].Count;
+
+            int num = bottom[0].Num;
+            int channels = bottom[0].Channels;
+            int height = bottom[0].Height;
+            int width = bottom[0].Width;            
+
+            Size padding = this.Parameters.Padding;
+            Size stride = this.Parameters.Stride;
+            Size kernel = this.Parameters.Kernel;
+
+            // Zero the output
+            bottomDiff.Map(v => 0, result: bottomDiff);
+
+            // Initialize 
+            Vector<float> inputMask;
+            bool useTopMask = top.Count > 1;
+            if (useTopMask)
+                inputMask = top[1].Data;
+            else
+                inputMask = this.maxIndexes;
+
+            // Main loop            
+            for (int index = 0; index < count; index++)
+            {
+                int w = index % width;
+                int h = (index / width) % height;
+                int c = (index / width / height) % channels;
+                int n = index / width / height / channels;
+
+                int phstart = (h + padding.Height < kernel.Height) ? 0 : (h + padding.Height - kernel.Height) / stride.Height + 1;
+                int phend = Math.Min((h + padding.Height) / stride.Height + 1, Pooled.Height);
+                int pwstart = (w + padding.Width < kernel.Width) ? 0 : (w + padding.Width - kernel.Width) / stride.Width + 1;
+                int pwend = Math.Min((w + padding.Width) / stride.Width + 1, Pooled.Width);
+
+                int topOffset = (n * channels + c) * Pooled.Height * Pooled.Width;
+
+                float bottomDatum = bottomData[index];
+
+                float gradient = 0;
+                for (int ph = phstart; ph < phend; ++ph)
+                {
+                    for (int pw = pwstart; pw < pwend; ++pw)
+                    {
+                        int topIndex = ph * Pooled.Width + pw;
+
+                        if (bottomDatum == topData[topOffset + topIndex])
+                            gradient += topDiff[topOffset + topIndex];
+                    }
+                }
+
+                bottomDiff[index] = gradient;
+            }
+        }
+    }
+}
